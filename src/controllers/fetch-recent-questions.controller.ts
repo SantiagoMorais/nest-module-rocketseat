4d5f57e05ecc42af9a2f5ee6a1f74d61bc9a4a9f
Felipe <fillikz@gmail.com>
2025-05-27 19:13:16 -0300

    feat(questions): add fetch recent questions endpoint with pagination support

---
+++ src/controllers/fetch-recent-questions.controller.ts
@@ -0,0 +1,33 @@
+import { JwtAuthGuard } from "@/auth/jwt-auth.guard";
+import {
+  pageQueryParamSchema,
+  TPageQueryParamSchema,
+} from "@/core/types/page-query-param";
+import { ZodValidationPipe } from "@/pipes/zod-validation-pipe";
+import { PrismaService } from "@/prisma/prisma.service";
+import { Controller, Get, Query, UseGuards } from "@nestjs/common";
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema);
+
+@Controller("/questions")
+@UseGuards(JwtAuthGuard)
+export class FetchRecentQuestionsController {
+  constructor(private prisma: PrismaService) {}
+
+  @Get()
+  async handle(
+    @Query("page", queryValidationPipe)
+    page: TPageQueryParamSchema
+  ) {
+    const perPage = 1;
+    const questions = await this.prisma.question.findMany({
+      take: perPage,
+      skip: (page - 1) * perPage,
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    return { questions };
+  }
+}
