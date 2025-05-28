e2760158e84fcc0717eb9cdbe09767338f3bdaf5
Felipe <fillikz@gmail.com>
2025-05-27 22:45:47 -0300

    feat(e2e): add dotenv configuration and setup for e2e tests

---
+++ src/controllers/create-account.controller.e2e-test.ts
@@ -0,0 +1,3 @@
+test("expect it works", () => {
+  expect(2).toEqual(2);
+});
--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     include: ['**/*.e2e-test.ts'],
     globals: true,
     root: "./",
+    setupFiles: ["./test/setup-e2e.ts"]
   },
   plugins: [
     swc.vite({
