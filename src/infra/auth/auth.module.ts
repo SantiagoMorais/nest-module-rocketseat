6a5973f8dbc526d93ec1d4cd8af85d450676ff99
Felipe <fillikz@gmail.com>
2025-06-03 15:51:46 -0300

    feat(env): introduce EnvModule and EnvService for environment variable management

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -1,21 +1,22 @@
-import { Env } from "@/infra/env";
 import { Module } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
+import { APP_GUARD } from "@nestjs/core";
 import { JwtModule } from "@nestjs/jwt";
 import { PassportModule } from "@nestjs/passport";
-import { JwtStrategy } from "./jwt.strategy";
+import { EnvService } from "../env/env.service";
 import { JwtAuthGuard } from "./jwt-auth.guard";
-import { APP_GUARD } from "@nestjs/core";
+import { JwtStrategy } from "./jwt.strategy";
+import { EnvModule } from "../env/env.module";
 
 @Module({
   imports: [
     PassportModule,
     JwtModule.registerAsync({
-      inject: [ConfigService],
+      imports: [EnvModule],
+      inject: [EnvService],
       global: true,
-      useFactory(config: ConfigService<Env, true>) {
-        const privateKey = config.get("JWT_PRIVATE_KEY", { infer: true });
-        const publicKey = config.get("JWT_PUBLIC_KEY", { infer: true });
+      useFactory(env: EnvService) {
+        const privateKey = env.get("JWT_PRIVATE_KEY");
+        const publicKey = env.get("JWT_PUBLIC_KEY");
         return {
           signOptions: { algorithm: "RS256" },
           privateKey: Buffer.from(privateKey, "base64"),
@@ -29,6 +30,7 @@ import { APP_GUARD } from "@nestjs/core";
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
     },
+    EnvService,
     JwtStrategy,
   ],
 })
--- src/infra/auth/jwt.strategy.ts
+++ src/infra/auth/jwt.strategy.ts
@@ -2,16 +2,15 @@ import {
   tokenPayloadSchema,
   TUserPayload,
 } from "@/core/types/token-payload-schema";
-import { Env } from "@/infra/env";
 import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { EnvService } from "../env/env.service";
 
 @Injectable() // Every provider must has this Decorator
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(config: ConfigService<Env, true>) {
-    const publicKey = config.get("JWT_PUBLIC_KEY", { infer: true });
+  constructor(config: EnvService) {
+    const publicKey = config.get("JWT_PUBLIC_KEY");
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: Buffer.from(publicKey, "base64"),
